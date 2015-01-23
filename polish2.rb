af054cde2a9556995c0441a0c8d8f5c7b366d429
Noah Holmes <noah.r.holmes@gmail.com>
2015-01-23 14:12:28 -0500

    temporary fix for issue

--- polish2.rb
+++ polish2.rb
@@ -1,10 +1,61 @@
+# class RPN
+
+#   def is_operator?(a)
+#     ["-", "/", "*", "+"].include?(a)
+#   end
+
+#   def is_number?(a)
+#     /\d/
+#   end
+
+#   def is_quit_command?(a)
+#     ["q", "quit", "exit"].include?(a)
+#   end
+
+#   def is_character?(a)
+#     /[a-zA-Z]/
+#   end
+
+#   def evaluation
+#     evaluation = []
+#     until evaluation.include?('q')
+#       symbols = []
+#       input = gets.chomp.split 
+#       input.each do |a|
+#       if is_operator?(a)
+#         if evaluation.size > 1
+#           symbols = evaluation.pop(2)
+#           evaluation.push(symbols[0].send(a, symbols[1])) 
+#         else 
+#           puts "Please enter more than one number first."
+#         end
+#       elsif is_number?(a)
+#         evaluation << a.to_f
+#       elsif is_quit_command?(a)
+#         puts "Quitting"
+#         exit
+#       elsif is_character?(a)
+#         puts "Please enter a number or valid operation."
+#         end
+#       end
+#      puts evaluation.last
+#     end
+#   end
+# end
+
+# RPN1 = RPN.new
+# RPN1.evaluation
+
+
+
 class RPN
+
   def is_operator?(a)
     ["-", "/", "*", "+"].include?(a)
   end
 
   def is_number?(a)
-    /\d/
+    true if Float(a) rescue false
   end
 
   def is_quit_command?(a)
@@ -41,5 +92,6 @@ class RPN
     end
   end
 end
+
 RPN1 = RPN.new
 RPN1.evaluation
