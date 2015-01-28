005a9fb6cd557191c2d9ec585e1fc39673632ab4
Noah Holmes <noah.r.holmes@gmail.com>
2015-01-28 13:50:42 -0500

    removed some unwanted code, changed variable and token names to reflect better on what they do

--- polish2.rb
+++ polish2.rb
@@ -1,62 +1,57 @@
 class RPN
 
   def initialize
-    @evaluation = []
-    @symbols = []
+    @stack = []
   end
 
-  def is_an_operator?(a)
-    ["-", "/", "*", "+"].include?(a)
+  def is_an_operator?(token)
+    ["-", "/", "*", "+"].include?(token)
   end
 
-  def calculate(a)
-    if @evaluation.size > 1
-      symbols = @evaluation.pop(2)
-      @evaluation.push(symbols[0].send(a, symbols[1])) 
+  def calculate(token)
+    if @stack.size > 1
+      symbols = @stack.pop(2)
+      @stack.push(symbols[0].send(token, symbols[1])) 
     else 
-      puts "Please enter more than one number first."
+      warn "Please enter more than one number first."
     end
   end
 
-  def is_a_number?(a)
-    a.match(/[0-9]/)
+  def is_a_number?(token)
+    token.match(/\d+(\.\d+)?/)
   end
 
-  def add_to_array(a)
-    @evaluation << a.to_f
+  def add_to_array(token)
+    @stack << token.to_f
   end
 
-  def is_quit_command?(a)
-    ["q", "quit", "exit"].include?(a)
-  end
-
-  def is_a_character?(a)
-    a.match(/[a-zA-Z]/)
+  def is_quit_command?(token)
+    ["q", "quit", "exit"].include?(token)
   end
 
   def evaluation_exists?
-    @evaluation.last != nil
+    @stack.last != nil
   end
 
   def evaluate
-    loop {
+    loop do
       input = gets.chomp.split 
-      input.each do |a|
-      if is_an_operator?(a)
-        calculate(a)
-      elsif is_a_number?(a)
-        add_to_array(a)
-      elsif is_quit_command?(a)
+      input.each do |token|
+      if is_an_operator?(token)
+        calculate(token)
+      elsif is_a_number?(token)
+        add_to_array(token)
+      elsif is_quit_command?(token)
         puts "Quitting"
         exit
-      elsif is_a_character?(a)
-        puts "Please enter a number or valid operation."
+      else
+        warn "Please enter a number or valid operation."
       end
       end
       if evaluation_exists?
-        puts @evaluation.last
+        puts @stack.last
       end
-    }
+    end
   end
 end
 
